f71464a21b040bb24387d4ab0d4098c28616adfa
Alexandre Angelim <angelim@angelim.com.br>
2011-07-28 06:45:50 -0300

    load models deprecated in mongoid

--- lib/voteable_mongo/railtie.rb
+++ lib/voteable_mongo/railtie.rb
@@ -2,13 +2,13 @@ module Rails #:nodoc:
   module VoteableMongo #:nodoc:
     class Railtie < Rails::Railtie #:nodoc:
 
-      initializer "preload all application models" do |app|
-        config.to_prepare do
-          if defined?(Mongoid)
-            ::Rails::Mongoid.load_models(app)
-          end
-        end
-      end
+      # initializer "preload all application models" do |app|
+      #   config.to_prepare do
+      #     if defined?(Mongoid)
+      #       ::Rails::Mongoid.load_models(app)
+      #     end
+      #   end
+      # end
 
       rake_tasks do
         load 'voteable_mongo/railties/database.rake'
